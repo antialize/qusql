287f4b04a610434251a2494ffd14b0f8291df754
Jakob Truelsen <jakob@scalgo.com>
2025-12-05 11:58:22 +0100

    Add module level documentation and top level exports.

--- qusql-mysql/src/lib.rs
+++ qusql-mysql/src/lib.rs
@@ -1,4 +1,65 @@
-//! This crate allow efficent async communication with mysql/mariadb
+//! This crate allow efficent async communication with Mariadb/Mysql
+//!
+//! You can either establish a raw connection using [Connection] or
+//! use a [Pool] of connections.
+//!
+//! Drop handling:
+//! ------------
+//!
+//! When dropping/cancelling any future or struct returned by the library. The connections
+//! will keep working. This is done by having the [Connection] have an internal state and
+//! finish up any partially executed queries when the next query is executed or when [Connection::cleanup]
+//! is called
+//!
+//! When dropping a [PoolConnection], any partial queries or transactions are finished up in
+//! a spawned task. If this task takes too long to execute the connection is closed an a new
+//! connection may be established.
+//!
+//! This means that if the sqly is used in a backend to handle web requests, and the
+//! web request is cancelled while performing a long running query. Then the long running
+//! query will be killed shortly after.
+//!
+//! Efficiency:
+//! -----------
+//! We spawn very few tasks.  Currently the only task spawned is
+//! when a [PoolConnection] is dropped while there is a ongoing query or transaction.
+//!
+//! We have few memory allocations. In the cause of a normal
+//! query, even one returning a string, no memory will be allocated
+//!
+//! The error types returned are all 8 bytes.
+//!
+//! Example:
+//! --------
+//! ```no_run
+//! use sqly2::{Pool, ConnectionOptions, PoolOptions, ConnectionError, ExecutorExt, Executor};
+//!
+//! async fn test() -> Result<(), ConnectionError> {
+//!     let pool = Pool::connect(
+//!         ConnectionOptions{
+//!             address: "127.0.0.1:3307".parse().unwrap(),
+//!             user: "user".into(),
+//!             password: "pw".into(),
+//!             database: "test".into(),
+//!             ..Default::default()
+//!         },
+//!         PoolOptions{
+//!              max_connections: 10,
+//!             ..Default::default()
+//!         }
+//!     ).await?;
+//!
+//!     let mut conn = pool.acquire().await?;
+//!
+//!     let mut tr = conn.begin().await?;
+//!     tr.execute("INSERT INTO `table` (`v`, `t`) VALUES (?)", (42, "test_string")).await?;
+//!     tr.commit().await?;
+//!
+//!     let rows: Vec<(i64, &str)> = conn.fetch_all("SELECT `v`, `t` FROM `table`", ()).await?;
+//!
+//!     Ok(())
+//! }
+//! ```
 pub mod args;
 pub mod auth;
 pub mod bind;
@@ -9,3 +70,9 @@ pub mod package_parser;
 pub mod plain_types;
 pub mod pool;
 pub mod row;
+
+pub use connection::{
+    Connection, ConnectionError, ConnectionErrorContent, ConnectionOptions, ExecuteResult,
+    Executor, ExecutorExt, Query, QueryIterator, Transaction,
+};
+pub use pool::{Pool, PoolConnection, PoolOptions};
