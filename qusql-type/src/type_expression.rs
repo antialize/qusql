caf38d34db5f47677eda361b0983a66261cd7031
Jakob Truelsen <jakob@scalgo.com>
2026-03-18 21:13:41 +0100

    parser: Add BETWEEN / NOT BETWEEN expression parsing

--- qusql-type/src/type_expression.rs
+++ qusql-type/src/type_expression.rs
@@ -567,5 +567,9 @@ pub(crate) fn type_expression<'a>(
             issue_todo!(typer.issues, e);
             FullType::invalid()
         }
+        e @ Expression::Between(_) => {
+            issue_todo!(typer.issues, e);
+            FullType::invalid()
+        }
     }
 }
